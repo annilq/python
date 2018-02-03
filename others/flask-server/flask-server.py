c934075b805e9f928dd0d99837dbbc7c0c94b957
annilq <410493517@qq.com>
2018-02-03 22:37:14 +0800

    连接数据库

--- others/flask-server/flask-server.py
+++ others/flask-server/flask-server.py
@@ -1,8 +1,8 @@
-from flask import Flask, url_for
-from flask import jsonify
-from flask import request
+from flask import Flask, url_for,redirect,jsonify,request
 from flask_pymongo import PyMongo
-
+from bson.json_util import dumps
+import json
+from user import User
 app = Flask(__name__)
 
 # connect to another MongoDB database on the same host
@@ -11,23 +11,49 @@ app.config['MONGO_URI'] = 'mongodb://localhost:27017/react-todos'
 
 MONGO = PyMongo(app)
 
-
 @app.route('/')
 def api_root():
-    output = []
+    return 'welcome to this place'
+
+@app.route('/api/users')
+def api_users():
     users = get_users()
     # return json format
     return jsonify({'result': users})
 
+@app.route('/api/tasks')
+def api_tasks():
+    tasks = get_tasks()
+    return jsonify({'result': tasks})
 
-@app.route('/articles')
-def api_articles():
-    return 'List of ' + url_for('api_articles')
+@app.route('/api/folders')
+def api_folders():
+    folders = get_folders()
+    return jsonify({'result': folders})
 
+@app.route('/api/register',methods=['POST'])
+def api_register():
+    userDb=User(MONGO.db)    
+    name = request.form.get('name', '')
+    email = request.form.get('email', '')
+    user=userDb.findUser(name)
+    if user:
+        return jsonify({ "code": -1, "message": "该用户已经注册过了" })
+    else:
+        data={'name':name,"email":email}
+        user=userDb.createUser(data)
+        if user:
+            print(type(user))
+            # ObjectId('5a75c691e566330f9a86a052') is not JSON serializable不能序列化是个问题啊
+            resp = app.make_response(dumps({ "code": 0,"data":user,"message": "注册成功" }))
+            resp.headers['Content-Type'] = 'application/json'
+            return resp
+        else:
+            jsonify({ code: -1, message: "注册失败" })
 
-@app.route('/articles/<articleid>')
-def api_article(articleid):
-    return 'You are reading ' + articleid
+@app.route('/<path:path>')
+def catch_all(path):
+    return  redirect(url_for('api_root'))
 
 # get users
 def get_users():
@@ -37,10 +63,29 @@ def get_users():
     users = MONGO.db.users.find()
     # convert to list
     for user in users:
-        print(user["name"])
         output.append({"name": user["name"], "email": user["email"]})
+    return output
+
+def get_folders():
+    """当前方法只能在flask的上下文中运行，单独执行会报错"""
+    output = []
+    # return a collection cursor
+    folders = MONGO.db.folders.find()
+    # convert to list
+    for folder in folders:
+        output.append({"userId":folder["userId"],"name":folder["name"]})
+    return output
+
+def get_tasks():
+    """当前方法只能在flask的上下文中运行，单独执行会报错"""
+    output = []
+    # return a collection cursor
+    tasks = MONGO.db.tasks.find()
+    # convert to list
+    for task in tasks:
+        output.append({"userId":task["userId"],"name":task["name"]})
     return output
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- others/flask-server/readme.md
+++ others/flask-server/readme.md
@@ -1,7 +1,12 @@
-1. 安装flask-pymongo
-```
+1. 首先安装mongodb
+参考[mongodb小记](https://annilq.github.io/2016/04/18/mongoDb/)
+2. 安装f依赖
+```python
+pip install Flask
 pip install Flask-PyMongo
+pip install flask-login 
 ```
 参考文档
 1. [pymongo](http://api.mongodb.com/python/current/tutorial.html)
-2. [flask-pymongo](http://flask-pymongo.readthedocs.io/en/latest/#flask_pymongo.PyMongo)
+2. [flask-pymongo ](http://flask-pymongo.readthedocs.io/en/latest/#flask_pymongo.PyMongo)
+3. [flask-login](https://flask-login.readthedocs.io/en/latest/)
